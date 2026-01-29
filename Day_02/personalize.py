1bbda9b50704e19da15c003ae6490e58ae9766d7
Ulrich Armel <armelulrich16@gmail.com>
2026-01-29 07:39:59 +0100

    feat(personalise message) : added day 2 solutions

---
+++ Day_02/personalize.py
@@ -0,0 +1,16 @@
+# personalize greeting program
+name = input("What is your name? ")
+age = input("How old are you? ")
+color = input("What is your favorite color? ")
+
+nickName = name [:3]
+
+
+print("\n---Personalized Greeting---")
+print(f"Hello, {name}, your nickName is {nickName}!")        
+print(f"You are {age} years old.")
+print(f"Your favorite color is {color}.")
+print("---------------------------")
+print(" welcome to the personalized greeting program!")
+
+print(" Enjoy your day!")
