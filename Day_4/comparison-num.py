807bd9fe5c561bf6164f798f00a35bc58e3c7ba7
Ulrich Armel <armelulrich16@gmail.com>
2026-02-13 08:59:37 +0100

    feat(complete) : completed day 4 challenge of adding comparison of 3 number and giving option to continue or break the program

--- Day_4/comparison-num.py
+++ Day_4/comparison-num.py
@@ -10,6 +10,44 @@ elif secondNum == firstNum:
 else:
     print("invalid input ")
 
-# challenge: compare 3 numbers, check if they are positive, include the option for user to run the
-# the peogram without restarting
+#challenge: compare 3 numbers, check if they are positive, include the option for user to run the
+t#he peogram without restarting
+
+
+while True:
+    print("-----Welcome to Number comparison program----")
+    firstNum =  float(input("Enter the first number \n"))
+    secondNum = float(input("Enter the second number \n"))
+    thirdNum = float(input("Enter the third number \n"))     
+  
     
+
+
+    checkType = [firstNum % 2, secondNum % 2, thirdNum % 2]
+
+    print(checkType)
+
+    if checkType[0] == 0 and checkType[1] == 0 and checkType[2] == 0 :
+        print(" All three numbers are even number ")
+        print([f"first Number : {firstNum}, Second Number : {secondNum} Third Number : {thirdNum}"])
+        startAgain = input("Do you want to continue ? ( yes / no )").lower()
+        if startAgain == "yes":
+            continue
+        else:
+            break
+    elif checkType[0] > 0 and checkType[1] > 0 and checkType[2] > 0 :
+        print(" All number enter are odd number ")
+        print(f" first number: {firstNum}, second Number : {secondNum}, third Number: {thirdNum}")
+        startAgain = input("Do you want to continue ? ( yes / no )").lower()
+        if startAgain == "yes":
+            continue
+        else:
+            break
+    else:
+        print("it contain both even and odd number ")
+        print(f"first Number: {firstNum}, second Number : {secondNum}, third Number: {thirdNum}")
+        startAgain = input("Do you want to continue ? ( yes / no )").lower()
+        if startAgain == "yes":
+            continue
+        else:
+            break
