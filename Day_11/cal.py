f7681612202365f5fe696702a078856387a674f1
Ulrich Armel <armelulrich16@gmail.com>
2026-04-04 08:54:02 +0100

    Added day 11 project , safe calculator

---
+++ Day_11/cal.py
@@ -0,0 +1,62 @@
+
+
+def add(x, y):
+    return x + y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    if y == 0:
+        raise ZeroDivisionError("ERROR: division by zero is not possible!")
+    return x / y
+def substract(x,y):
+    return x - y
+
+def modular(x,y):
+    return x // y
+
+def raiseToPower(x,y):
+    return x**y
+
+def showMenu():
+    print("---- Safe Calculator----")
+    print("1. Add")
+    print("2. Divide")
+    print("3. Multiply")
+    print("4. Substract")
+    print("5. Modular division")
+    print("6. Raise to the power")
+    print("7. Exit")
+
+
+while True:
+    showMenu()
+    choice = input(" Enter your choice !")
+    if choice == "7":
+        print("Thank you for using safe calculator")
+        print("Bye!")
+        break
+
+    try:
+        x = int(input("Enter the first number!"))
+        y = int(input("Enter the second number!"))
+        if choice == "1":
+            print("Result: " , add(x, y))
+        elif choice == "2":
+            print("Result: ", divide(x, y))
+        elif choice == "3":
+            print("Result :",  multiply(x, y))
+        elif choice == "4":
+            print("Result: ", substract(x, y))
+        elif choice == "5":
+            print("Result :", modular(x,y))
+        elif choice == "6":
+            print("Result: ",    raiseToPower(x, y))
+        elif choice == "7":
+            print("Bye Thank you for using safe calculator")
+            break
+    except ValueError:
+        print("Error : Invalid input ")
+    except ZeroDivisionError:
+        print("Division by zero not possible")
