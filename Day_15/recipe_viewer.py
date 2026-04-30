db45d32e757f99e0b9544b42a27c887aca04885c
Ulrich Armel <armelulrich16@gmail.com>
2026-04-30 14:07:08 +0100

    fix-bug: fix eror that distort the display of output

---
+++ Day_15/recipe_viewer.py
@@ -0,0 +1,56 @@
+# Recipe Viewer App
+# step 1 : load recipe
+def load_recipe(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            recipes = content.split('\n\n')
+            recipe_dic = {}
+            for recipe in recipes:
+                lines = recipe.strip().split('\n')
+                if len(lines) >= 3:
+                    name = lines[0].strip()
+                    ingredients = lines[1].replace('Ingredients:', '').strip()
+                    instructions = lines[2].replace('Instructions:', '').strip()
+                  
+                    recipe_dic[name] = {"ingredients": ingredients, "instructions": instructions}
+            return recipe_dic
+    except FileNotFoundError:
+        print("Error: File not found!!")
+        return {} # Return empty dict to prevent errors later
+
+# step 2 Display Recipe Menu
+def show_menu():
+    print("\n--- Recipe Viewer Menu---")
+    print("1. View All Recipes")
+    print("2. View Recipe by Name")
+    print("3. Exit App")
+  
+
+# step 3 display recipe details
+def view_recipe(recipes):
+    name = input("Enter the name of the recipe : ").strip()
+    if name in recipes:
+        print(f"\n---- Recipe {name} Details---")
+        print(f"Ingredients: {recipes[name]['ingredients']}")
+        print(f"Instructions: {recipes[name]['instructions']} ")
+    else:
+        print("Recipe not found")
+
+# step 4 : Main program
+recipe_file = 'recipe.txt'
+recipes = load_recipe(recipe_file)
+
+if recipes is not None:
+    while True:
+        show_menu()
+        choice = input("Enter your choice [1/2/3]: ")
+        if choice == '1':
+            print("\n--All Recipes--")
+            for name in recipes:
+                print(f"- {name}")
+        elif choice == '2':
+            view_recipe(recipes)
+        elif choice == '3':
+            print("Exiting program!")
+            break
